08f684cb0d968e4151107850a8abd082f0019817
Andrei Nazarov <andrei.nazarov@tum.de>
2024-01-06 16:21:12 +0100

    Solved lab

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,23 +1,59 @@
 import { useState } from "react";
+import styles from "../styles/travelList.module.css";
 
-function TravelList ({travelPlansData}) {
-    const [plans, setPlans] = useState(travelPlansData);
+function TravelList({ travelPlansData }) {
+  const [plans, setPlans] = useState(travelPlansData);
+  const deletPlan = (planId) => {
+    const filteredPlans = plans.filter((plan) => {
+      return plan.id !== planId;
+    });
 
-    return (
-        <div className="list">
-            {plans.map((onePlan, id) => {
-                return (
-                <div className="card"> 
-                   <img src='{onePlan.image}'/>
-                   <div className='cardText'>
-                   <h4 key="{onePlan.id}">{onePlan.destination}</h4>;
-                   <p>{onePlan.description}</p>
-                   <p><b>Price:</b> {onePlan.price} €</p>
-                   </div> 
-                </div>)
-            })}
-        </div>
-    )
+    setPlans(filteredPlans);
+  };
+
+  return (
+    <div className={styles.list}>
+      {plans.map((onePlan) => {
+        return (
+          <div className={styles.card} key={onePlan.id}>
+            <img className={styles.img} src={onePlan.image} />
+            <div className={styles.text}>
+              <h3>
+                {onePlan.destination}, ({onePlan.days} days)
+              </h3>
+              <p>{onePlan.description}</p>
+              <p>
+                <b>Price:</b> {onePlan.totalCost} €
+              </p>
+              <div className={styles.labels}>
+                {onePlan.totalCost <= 350 && (
+                  <div className={styles.labelGreen}>
+                    <p>Great Deal</p>
+                  </div>
+                )}
+                {onePlan.totalCost >= 1500 && (
+                  <div className={styles.labelBlue}>
+                    <p>Premium</p>
+                  </div>
+                )}
+                {onePlan.allInclusive && (
+                  <div className={styles.labelBlue}>
+                    <p>All-Inclusive</p>
+                  </div>
+                )}
+              </div>
+              <button
+                onClick={() => deletPlan(onePlan.id)}
+                className={styles.btnDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default TravelList;
